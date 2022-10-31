b1a8e29a28e3f7f55c03e0d5be8cc11f3b739be9
Emily Pisaygnane <episaygnane@me.com>
2022-10-31 16:51:34 -0700

    first test passed for getting id and name for each zodiac, and created bare bones off app

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../lib/zodiac-data');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('zodiac routes', () => {
+  it('/zodiacs should return a list of zodiac signs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
