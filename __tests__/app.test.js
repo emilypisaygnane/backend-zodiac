c76ba2243d9cb1b415e10381f309a9bd3500b3cc
Emily Pisaygnane <episaygnane@me.com>
2022-10-31 17:02:02 -0700

    test two passed for returning details

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,4 +10,15 @@ describe('zodiac routes', () => {
     });
     expect(res.body).toEqual(expected);
   });
+
+  it('/zodiacs/:id should return zodiac details', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer'
+    };
+    expect(res.body).toEqual(aquarius);
+  });
 });
