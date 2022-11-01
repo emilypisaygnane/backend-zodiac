cf5a4b8eb7fa2bd296fc439e35b7c85ffdd9f8d0
Emily Pisaygnane <episaygnane@me.com>
2022-11-01 15:45:40 -0700

    added test 2, test should return details

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,16 @@ describe('zodiac routes', () => {
       return { id: zodiac.id, name: zodiac.name };
     });
     expect(res.body).toEqual(expected);
+  })
+
+  it('/zodiacs/:id should return zodiac details', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer'
+    };
+    expect(res.body).toEqual(aquarius);
   });
-
-
 });
