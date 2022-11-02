a5a25b5897f30aa2f37314cc47d790b459fd84b2
Emily Pisaygnane <episaygnane@me.com>
2022-11-01 17:38:41 -0700

    removed horoscopes from data file, changed test for horoscope

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,17 +18,18 @@ describe('zodiac routes', () => {
       name: 'aquarius',
       dates: 'Jan 21 - Feb 19',
       symbol: 'Water Bearer',
-      horoscope: 'The planet Saturn, which rules government structures and responsibility, is in your sign this year, Aquarius. You are the rebel of the zodiac, and as the water-bearer, it is your job to fight injustice. 2022 sees everyone else trying to get on your level thanks to your influence on the usually-stodgy Saturn. But while you are out there trying to save the world, Aquarius, do not forget about yourself. And you need to pay attention to your personal life, because when it comes to money and love, 2022 is one for the record books.'
-
     };
     expect(res.body).toEqual(aquarius);
   });
 
-  it('/horoscope/:sign should return horoscope for each zodiac', async () => {
-    const res = await request(app).get('/horoscope/libra');
-    const libra = {
-      horoscope:'When 2022 begins, your ruling planet, Venus, is retrograde. Because the planet rules love, beauty, and abundance, it is easier to feel as if you shine dimmer during this time. But you don not at all; it is just a time to regroup and re-establish your goals regarding love and money. Because when the year ends, with marriage-oriented asteroid Ceres in your sign, you will feel like the shiniest earthling in the world.'
+  describe('horoscope routes', () => {
+    it('/horoscope/:name should return horoscope for each zodiac', async () => {
+      const res = await request(app).get('/horoscope/aquarius');
+      const aquarius = {
+        sign: 'aquarius',
+        date: '2022-11-01',
+        horoscope:'You may have a hard time appreciating the little things this morning, dear Aquarius, as the moon forms a harsh square with the nodes of fate. Certain lifestyle luxuries, personal success, and status may not taste as sweet as you would hoped, causing you to close off emotionally. Don not be hard on yourself if you are feeling disenchanted with the world, but try to reach for that which brings you joy. The vibe will be charged within your domestic affairs when Luna squares off with Uranus this afternoon, so you will want to be on guard for finicky appliances or temperamental roommates.'
     };
-    expect(res.body).toEqual(libra);
+    expect(res.body).toEqual(aquarius);
   });
 });
